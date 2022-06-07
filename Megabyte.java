7e84fb8e9363c1e1ae1f05ac3f8c3b61b5c26e96
AJ MICHAEL <84230323+ajmichael521@users.noreply.github.com>
2022-06-07 12:28:49 +0100

    Update Megabyte.java

--- Megabyte.java
+++ Megabyte.java
@@ -1,9 +1,17 @@
 
 public class Megabyte {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void printMegaBytesAndKilobyte (int kiloByte) {
+		//Parameter if kilobyte is less than zero
+		if(kiloByte < 0) {
+			System.out.println("invalid value");
+		} else {
+		//Expression to get any amount megabyte
+			int megaByte = (kiloByte/1024);
+			//Expression to get the remainder of megabyte to kilobyte
+			int kiloRemainder = (kiloByte%1024);
+			System.out.println(kiloByte + "KB = " + megaByte + "MB and " + kiloRemainder + "KB");
+		}
 	}
-
+	
 }
